ed46cfb30112a3214a85ae748eb282f2305f86a3
Legas <00legas00@gmail.com>
2023-06-11 19:13:17 +0300

    Dependency injection с помощью сеттера

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,9 +1,20 @@
 package spring_introduction;
 
 public class Person {
+    public void setPet(Pet pet) {
+        System.out.println("set pet");
+        this.pet = pet;
+    }
+
     private Pet pet;
 
+    public Person() {
+        System.out.println("Bean Person is created");
+    }
+
+    // pet -> setPet()
     public Person(Pet pet) {
+        System.out.println("Bean Person is created");
         this.pet = pet;
     }
 
