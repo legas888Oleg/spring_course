f45bc2192d0d728a9f3de95b34004a9e882f28e2
Legas <00legas00@gmail.com>
2023-06-11 21:10:07 +0300

    Методы init и destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,7 +1,7 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
+//    private String name;
     public Dog() {
         System.out.println("Bean dog is created");
     }
@@ -11,11 +11,18 @@ public class Dog implements Pet {
         System.out.println("Гав-гав");
     }
 
-    public String getName() {
-        return name;
+    public void init(){
+        System.out.println("Class Dog: init method");
+    }
+    public void destroy(){
+        System.out.println("Class Dog: destroy method");
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -8,12 +8,13 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("application_context1.xml");
         Dog pet = context.getBean("myPet", Dog.class);
-        pet.setName("Belka");
-        Dog pet1 = context.getBean("myPet", Dog.class);
-        pet1.setName("Strelka");
-
-        System.out.println(((Dog) pet).getName());
-        System.out.println(((Dog) pet1).getName());
+        pet.say();
+//        pet.setName("Belka");
+//        Dog pet1 = context.getBean("myPet", Dog.class);
+//        pet1.setName("Strelka");
+//
+//        System.out.println(((Dog) pet).getName());
+//        System.out.println(((Dog) pet1).getName());
 
         context.close();
     }
