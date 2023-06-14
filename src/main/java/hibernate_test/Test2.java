e38d1d3f86728755888f0d83bcd14daf7624e294
Legas <00legas00@gmail.com>
2023-06-14 14:13:32 +0300

    Получение объекта по id

--- src/main/java/hibernate_test/Test2.java
+++ src/main/java/hibernate_test/Test2.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -15,10 +15,20 @@ public class Test1 {
         try {
             Session session = factory.getCurrentSession();
 
-            Employee employee = new Employee("Алексей", "Никитин", "OK", 30000);
+            Employee employee = new Employee("Anastasia", "Grekova", "OK", 75000);
             session.beginTransaction();
-            session.save(employee);
+            session.persist(employee);
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+            int myId = employee.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee employee1 = session.get(Employee.class, myId);
             session.getTransaction().commit();
+
+            System.out.println(employee1);
+
         } finally {
             factory.close();
         }
