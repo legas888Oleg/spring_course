42d6ebef26d01a1edf7761946a3a1bb8a6890584
Legas <00legas00@gmail.com>
2023-06-15 11:02:14 +0300

    Аннотация @RequestParam

--- src/main/java/ru/legas/spring/mvc/MyController.java
+++ src/main/java/ru/legas/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package ru.legas.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,9 +24,19 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName,
+                                      Model model){
+
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
 
