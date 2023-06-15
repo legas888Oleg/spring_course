6938c927834da8599ea380583ca80093277f2fa8
Legas <00legas00@gmail.com>
2023-06-15 11:07:29 +0300

    Аннотация @RequestMapping для Controller класса

--- src/main/java/ru/legas/spring/mvc/MyController.java
+++ src/main/java/ru/legas/spring/mvc/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping("/")
     public String showFirstView(){
@@ -18,20 +19,6 @@ public class MyController {
     public String askEmployeeDetails(){
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model){
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
     public String showEmployeeDetails(@RequestParam("employeeName") String empName,
