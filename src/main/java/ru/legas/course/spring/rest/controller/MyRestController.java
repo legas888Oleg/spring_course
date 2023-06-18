651150e2873ea36ce04c9211712bcc5e59360e73
Legas <00legas00@gmail.com>
2023-06-18 19:53:34 +0300

    Глобальная обработка исключений

--- src/main/java/ru/legas/course/spring/rest/controller/MyRestController.java
+++ src/main/java/ru/legas/course/spring/rest/controller/MyRestController.java
@@ -1,11 +1,8 @@
 package ru.legas.course.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.legas.course.spring.rest.entity.Employee;
-import ru.legas.course.spring.rest.exception_handler.EmployeeIncorrectData;
 import ru.legas.course.spring.rest.exception_handler.NoSuchEmployeeException;
 import ru.legas.course.spring.rest.service.EmployeeService;
 
@@ -34,21 +31,5 @@ public class MyRestController {
         }
 
         return employee;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handlerException(NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handlerException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
