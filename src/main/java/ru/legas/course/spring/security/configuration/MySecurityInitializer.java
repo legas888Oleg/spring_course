77669ae84bb9f031e68a2238b2ab6cf5edf2a418
Legas <00legas00@gmail.com>
2023-06-19 09:41:49 +0300

    Конфигурация приложения

---
+++ src/main/java/ru/legas/course/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package ru.legas.course.spring.security.configuration;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
