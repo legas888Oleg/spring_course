2a2621b32f9e16c74eda9d06714694b7a3ecf8f8
Legas <00legas00@gmail.com>
2023-06-21 11:25:58 +0300

    Изменение EmployeeServiceImpl под работу c JPA

--- src/main/java/ru/legas/course/spring/spring_data_jpa/service/EmployeeServiceImpl.java
+++ src/main/java/ru/legas/course/spring/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -2,38 +2,39 @@ package ru.legas.course.spring.spring_data_jpa.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.legas.course.spring.spring_data_jpa.dao.EmployeeRepository;
 import ru.legas.course.spring.spring_data_jpa.entity.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
-    private EmployeeRepository employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Override
-    @Transactional
     public List<Employee> getAllEmployees() {
-        return employeeDAO.getAllEmployees();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public void saveEmployee(Employee employee) {
-        employeeDAO.saveEmployee(employee);
+        employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public Employee getEmployee(int id) {
-        return employeeDAO.getEmployee(id);
+        Employee employee = null;
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if(optional.isPresent())
+            employee = optional.get();
+
+        return employee;
     }
 
     @Override
-    @Transactional
     public void deleteEmployee(int id) {
-        employeeDAO.deleteEmployee(id);
+        employeeRepository.deleteById(id);
     }
 }
